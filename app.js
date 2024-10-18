1cd986c71e51f67fa2d08ca05fc0dc9dc8e802ab
Varun S <varsho2002@gmail.com>
2024-10-18 16:10:12 +0530

    Add Error controller

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
